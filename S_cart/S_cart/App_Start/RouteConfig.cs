a0ab700b32b230f6ac79b741ee2b919adecb55cb
judyssc <judycss2007@gmail.com>
2019-04-03 01:09:16 +0800

    layout & partial views

--- S_cart/S_cart/App_Start/RouteConfig.cs
+++ S_cart/S_cart/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace S_cart
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
---
+++ S_cart/S_cart/Controllers/PartialViewsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using S_cart.Models;
+
+namespace S_cart.Controllers
+{
+    public class PartialViewsController : Controller
+    {
+        // GET: PartialViews
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViews(string page)
+        {
+            //initializing a User object and setting values - to be used in layout page
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "first name"; //need to call method from UserData
+            user.LastName = "last name";
+            user.Username = "username";
+            user.Password = "password";
+            user.SessionId = "123";
+            ViewData["user"] = user;
+
+            //using ViewData, pass the object with parameter to view
+            
+            ViewData["type"] = page; 
+            return View();
+        }
+    }
+}
