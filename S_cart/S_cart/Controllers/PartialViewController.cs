f9c0dce44b4653f9d38b86bd9c04a0857d057dc5
judyssc <judycss2007@gmail.com>
2019-04-02 17:03:29 +0800

    changes

---
+++ S_cart/S_cart/Controllers/PartialViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using S_cart.Models;
+
+namespace S_cart.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViews(string title)
+        {
+            User user = new User();
+            user.FirstName = "firstname";
+            user.LastName = "lastname";
+            ViewData["user"] = user;
+
+            return View(); 
+        }
+    }
+}
