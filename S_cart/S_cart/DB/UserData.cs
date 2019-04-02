d8ec8f1434c048687941d4f6712b635bec64ac64
qingshuangchan <qingshuang.chan@gmail.com>
2019-04-02 17:01:23 +0800

    Login changes

--- S_cart/S_cart/DB/UserData.cs
+++ S_cart/S_cart/DB/UserData.cs
@@ -8,9 +8,34 @@ using System.Diagnostics;
 
 namespace S_cart.DB
 {
+    //Handle all queries relating to user
     public class UserData : Data
     {
-        //Handle all queries relating to user
+        public static User GetUserByUsername(string username)
+        {
+            User user = null;
 
+            //Able to use connection string freely as UserData is inherited
+            //from Data parent class
+            using (SqlConnection conn = new SqlConnection(Data.connectionString))
+            {
+                conn.Open();
+
+                string sql = @"SELECT user_id, username, password from user_info
+                    WHERE firstname = '" + username + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    user = new User()
+                    {
+                        Id = (int)reader["user_id"],
+                        Password = (string)reader["password"]
+                    };
+                }
+            }
+            return user;    //pass to calling function (Login controller)
+        }
     }
 }
